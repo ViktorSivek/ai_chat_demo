248ce7b6baf46699761e5fa44c1d8926d681e38b
Viktor Sívek <sivv01@vse.cz>
2024-01-21 14:20:12 +0100

    clerk auth

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "AIDoprava - Dashboard",
+};
+
+export default function DashboardPage() {
+    return <div>Here will be dashboard</div>
+}
