0ff124cc4f8d7562b5a78d8f62b0c3317ddc8757
Viktor Sívek <sivv01@vse.cz>
2024-01-30 21:08:54 +0100

    omg its mby rdy

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,14 +1,14 @@
-import { SignIn } from "@clerk/nextjs"
+import { SignIn } from "@clerk/nextjs";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "AIDoprava - Sign In",
+  title: "AIDoprava - Sign In",
 };
 
 export default function SignInPage() {
-    return(
-        <div className="flex h-screen items-center justify-center">
-            <SignIn appearance={{ variables: { colorPrimary: "#0F172A" }}} />
-        </div>
-    )
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <SignIn appearance={{ variables: { colorPrimary: "#0F172A" } }} />
+    </div>
+  );
 }
---
+++ utils/parseXML.ts
@@ -0,0 +1,74 @@
+import xml2js from "xml2js";
+
+export type parsedItem = {
+  MTXT?: string;
+  StartTime?: string;
+  EndTime?: string;
+  OTXT?: string;
+  TXPL?: string;
+  SBEG?: { x: string; y: string };
+};
+
+export const parseXmlAndExtractTag = async (
+  xmlData: string,
+): Promise<parsedItem[]> => {
+  const parser = new xml2js.Parser({ explicitArray: false });
+  let result;
+  try {
+    result = await parser.parseStringPromise(xmlData);
+  } catch (error) {
+    console.error("Error parsing XML:", error);
+    return [];
+  }
+
+  if (!result?.DInfo?.XML) {
+    console.error("Unexpected XML structure:", result);
+    return [];
+  }
+
+  const xmlArray = Array.isArray(result.DInfo.XML)
+    ? result.DInfo.XML
+    : [result.DInfo.XML];
+  const parsedItems: parsedItem[] = [];
+
+  xmlArray.forEach((xmlEntry: any) => {
+    if (xmlEntry?.DOC?.MJD?.MSG) {
+      const msgs = Array.isArray(xmlEntry.DOC.MJD.MSG)
+        ? xmlEntry.DOC.MJD.MSG
+        : [xmlEntry.DOC.MJD.MSG];
+      msgs.forEach((msg: any) => {
+        const parsedItem: parsedItem = {};
+
+        if (msg?.MTXT?._) {
+          parsedItem.MTXT = msg.MTXT._;
+        }
+        if (msg?.MTIME?.TSTA?.$) {
+          const startDate = msg.MTIME.TSTA.$.date || "defaultDate";
+          const startTime = msg.MTIME.TSTA.$.time || "defaultTime";
+          parsedItem.StartTime = `${startDate} ${startTime}`;
+        }
+        if (msg?.MTIME?.TSTO?.$) {
+          const endDate = msg.MTIME.TSTO.$.date || "defaultDate";
+          const endTime = msg.MTIME.TSTO.$.time || "defaultTime";
+          parsedItem.EndTime = `${endDate} ${endTime}`;
+        }
+        if (msg?.MEVT?.OTXT?._) {
+          parsedItem.OTXT = msg.MEVT.OTXT._;
+        }
+        if (msg?.MLOC?.TXPL) {
+          parsedItem.TXPL = msg.MLOC.TXPL;
+        }
+        if (msg?.MLOC?.SNTL?.SBEG) {
+          parsedItem.SBEG = {
+            x: msg.MLOC.SNTL.SBEG.x,
+            y: msg.MLOC.SNTL.SBEG.y,
+          };
+        }
+
+        parsedItems.push(parsedItem);
+      });
+    }
+  });
+
+  return parsedItems;
+};
